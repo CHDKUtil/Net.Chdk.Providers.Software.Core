828d215940a3dc6689343fa8c0e4d087b956e2a3
Dmitry Shechtman <damnian@gmail.com>
2017-05-10 21:28:02 +0300

    Add ISourceProvider

---
+++ src/Net.Chdk.Providers.Software.Core/ISourceProvider.cs
@@ -0,0 +1,9 @@
+﻿using Net.Chdk.Model.Software;
+
+namespace Net.Chdk.Providers.Software
+{
+    public interface ISourceProvider
+    {
+        SoftwareSourceInfo GetSource(string productName, string sourceName, string channelName);
+    }
+}
