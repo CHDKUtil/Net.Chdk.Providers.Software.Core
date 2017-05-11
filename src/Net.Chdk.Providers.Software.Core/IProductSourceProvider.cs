0c6dd8d8c321abc7e3722219b8c928c9e55e2fbb
Dmitry Shechtman <damnian@gmail.com>
2017-05-11 23:01:36 +0300

    Add IProductSourceProvider

---
+++ src/Net.Chdk.Providers.Software.Core/IProductSourceProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Model.Software;
+using System.Collections.Generic;
+
+namespace Net.Chdk.Providers.Software
+{
+    public interface IProductSourceProvider
+    {
+        IEnumerable<KeyValuePair<string, SoftwareSourceInfo>> GetSources(SoftwareProductInfo product);
+        IEnumerable<SoftwareSourceInfo> GetSources(SoftwareProductInfo product, string sourceName);
+    }
+}
