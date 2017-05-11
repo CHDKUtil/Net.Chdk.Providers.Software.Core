9690d5738057d2fd509c3da346a9f982f23dad56
Dmitry Shechtman <damnian@gmail.com>
2017-05-11 18:36:25 +0300

    Add ISoftwareHashProvider

---
+++ src/Net.Chdk.Providers.Software.Core/ISoftwareHashProvider.cs
@@ -0,0 +1,10 @@
+﻿using Net.Chdk.Model.Software;
+using System.IO;
+
+namespace Net.Chdk.Providers.Software
+{
+    public interface ISoftwareHashProvider
+    {
+        SoftwareHashInfo GetHash(Stream stream, string fileName, string hashName);
+    }
+}
