d835c7fd23e403f3424745f628e2afb1fdcb272e
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 05:19:07 +0300

    Replace GetEncodingVersion() with GetCamera()

---
+++ src/Net.Chdk.Providers.Software.Core/CameraModel.cs
@@ -0,0 +1,12 @@
+﻿using Net.Chdk.Model.Camera;
+using Net.Chdk.Model.CameraModel;
+
+namespace Net.Chdk.Providers.Software
+{
+    public sealed class CameraModel
+    {
+        public CameraInfo Info { get; set; }
+        public CameraModelInfo Model { get; set; }
+        public int? EncodingVersion { get; set; }
+    }
+}
