256fefed2da18ac33b8f2b931d61b741d1072a6b
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 14:40:04 +0300

    Remove Children

--- src/Net.Chdk.Providers.Software.Core/IModuleProvider.cs
+++ src/Net.Chdk.Providers.Software.Core/IModuleProvider.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace Net.Chdk.Providers.Software
+﻿namespace Net.Chdk.Providers.Software
 {
     public interface IModuleProvider
     {
         string Path { get; }
         string Extension { get; }
-        IDictionary<string, ModuleData> Children { get; }
 
         string GetModuleName(string filePath);
         string GetModuleTitle(string name);
