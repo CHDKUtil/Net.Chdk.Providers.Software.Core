2309d42f21a397794f434651d31cbad566b7a665
Dmitry Shechtman <damnian@gmail.com>
2017-06-03 21:47:59 +0300

    Add GetSources(SoftwareCategoryInfo)

--- src/Net.Chdk.Providers.Software.Core/IProductSourceProvider.cs
+++ src/Net.Chdk.Providers.Software.Core/IProductSourceProvider.cs
@@ -5,6 +5,7 @@ namespace Net.Chdk.Providers.Software
 {
     public interface IProductSourceProvider
     {
+        IEnumerable<KeyValuePair<string, SoftwareSourceInfo>> GetSources(SoftwareCategoryInfo category);
         IEnumerable<KeyValuePair<string, SoftwareSourceInfo>> GetSources(SoftwareProductInfo product);
         IEnumerable<SoftwareSourceInfo> GetSources(SoftwareProductInfo product, string sourceName);
     }
--- src/Net.Chdk.Providers.Software.Core/ISourceProvider.cs
+++ src/Net.Chdk.Providers.Software.Core/ISourceProvider.cs
@@ -5,6 +5,7 @@ namespace Net.Chdk.Providers.Software
 {
     public interface ISourceProvider
     {
+        IEnumerable<KeyValuePair<string, SoftwareSourceInfo>> GetSources(SoftwareCategoryInfo category);
         IEnumerable<KeyValuePair<string, SoftwareSourceInfo>> GetSources(SoftwareProductInfo product);
         IEnumerable<SoftwareSourceInfo> GetSources(SoftwareProductInfo product, string sourceName);
     }
