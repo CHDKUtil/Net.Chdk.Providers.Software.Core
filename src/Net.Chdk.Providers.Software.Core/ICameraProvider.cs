7f8f48c18b9313e3795a9df2f0a0eb542894b4a2
Dmitry Shechtman <damnian@gmail.com>
2017-07-03 05:42:14 +0300

    Remove IEncodingProvider Add GetEncoding() to ICameraProvider

--- src/Net.Chdk.Providers.Software.Core/ICameraProvider.cs
+++ src/Net.Chdk.Providers.Software.Core/ICameraProvider.cs
@@ -7,5 +7,6 @@ namespace Net.Chdk.Providers.Software
     public interface ICameraProvider
     {
         SoftwareCameraInfo GetCamera(SoftwareProductInfo productInfo, CameraInfo cameraInfo, CameraModelInfo cameraModelInfo);
+        SoftwareEncodingInfo GetEncoding(SoftwareProductInfo productInfo, SoftwareCameraInfo cameraInfo);
     }
 }
--- src/Net.Chdk.Providers.Software.Core/IEncodingProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Net.Chdk.Model.Software;
-
-namespace Net.Chdk.Providers.Software
-{
-    public interface IEncodingProvider
-    {
-        SoftwareEncodingInfo GetEncoding(SoftwareProductInfo product, SoftwareCameraInfo camera);
-    }
-}
