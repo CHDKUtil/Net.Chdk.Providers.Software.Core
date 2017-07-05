495256ceac4fb7a9d0b74a9789505e152393b35d
Dmitry Shechtman <damnian@gmail.com>
2017-07-05 06:41:05 +0300

    Override Equals() and GetHashCode() in ProductSource

--- src/Net.Chdk.Providers.Software.Core/ProductSource.cs
+++ src/Net.Chdk.Providers.Software.Core/ProductSource.cs
@@ -14,5 +14,18 @@ namespace Net.Chdk.Providers.Software
         public string ProductName { get; }
         public string SourceName { get; }
         public SoftwareSourceInfo Source { get; }
+
+        public override bool Equals(object obj)
+        {
+            var source2 = obj as ProductSource;
+            if (source2 == null)
+                return false;
+            return Source.Equals(source2.Source);
+        }
+
+        public override int GetHashCode()
+        {
+            return Source.GetHashCode();
+        }
     }
 }
