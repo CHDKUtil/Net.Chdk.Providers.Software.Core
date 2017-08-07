c2ac9f48d67a01c4e0e7961344c1f9cd7f1cfed2
Dmitry Shechtman <damnian@gmail.com>
2017-08-07 22:10:06 +0300

    Remove GetHash(Stream, string, string)

--- src/Net.Chdk.Providers.Software.Core/ISoftwareHashProvider.cs
+++ src/Net.Chdk.Providers.Software.Core/ISoftwareHashProvider.cs
@@ -1,11 +1,9 @@
 ﻿using Net.Chdk.Model.Software;
-using System.IO;
 
 namespace Net.Chdk.Providers.Software
 {
     public interface ISoftwareHashProvider
     {
-        SoftwareHashInfo GetHash(Stream stream, string fileName, string hashName);
         SoftwareHashInfo GetHash(byte[] buffer, string fileName, string hashName);
     }
 }
